5b8f6409256abe56e2be806dd654905faa2c3f95
2unmi <2unmin4030@gmail.com>
2025-05-20 13:26:33 +0900

    [Docs] Swagger 설정 - 계좌 관련 api 명세서 작성

--- src/main/java/com/doubleowner/revibe/domain/account/controller/AccountController.java
+++ src/main/java/com/doubleowner/revibe/domain/account/controller/AccountController.java
@@ -5,6 +5,9 @@ import com.doubleowner.revibe.domain.account.dto.AccountResponseDto;
 import com.doubleowner.revibe.domain.account.service.AccountService;
 import com.doubleowner.revibe.global.common.dto.CommonResponseBody;
 import com.doubleowner.revibe.global.config.auth.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,10 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
+    @Operation(summary = "계좌 추가 API",description = "계좌를 추가 할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "계좌를 추가하면 201 CREATED")
+    })
     public ResponseEntity<CommonResponseBody<AccountResponseDto>> createAccount(
             @Valid @RequestBody AccountRequestDto dto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -32,6 +39,10 @@ public class AccountController {
     }
 
     @GetMapping
+    @Operation(summary = "계좌 조회 API",description = "계좌를 조회 할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "계좌를 조회하면 200 OK")
+    })
     public ResponseEntity<CommonResponseBody<AccountResponseDto>> findAccount(
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
@@ -41,6 +52,10 @@ public class AccountController {
     }
 
     @PatchMapping
+    @Operation(summary = "계좌 수정 API",description = "계좌를 수정 할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "계좌 수정 성공 하면 200 OK")
+    })
     public ResponseEntity<CommonResponseBody<AccountResponseDto>> updateAccount(
             @RequestBody AccountRequestDto dto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -51,6 +66,10 @@ public class AccountController {
     }
 
     @DeleteMapping
+    @Operation(summary = "계좌 삭제 API",description = "계좌를 삭제 할수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "계좌를 삭제 성공 하면 200 OK")
+    })
     public ResponseEntity<CommonResponseBody<Void>> delete(
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
--- src/main/java/com/doubleowner/revibe/domain/account/dto/AccountRequestDto.java
+++ src/main/java/com/doubleowner/revibe/domain/account/dto/AccountRequestDto.java
@@ -1,5 +1,6 @@
 package com.doubleowner.revibe.domain.account.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -8,10 +9,12 @@ import lombok.Getter;
 public class AccountRequestDto {
 
     @NotBlank(message = "은행 이름은 필수 값입니다")
+    @Schema(defaultValue = "농협",description = "은행명")
     private String bank;
 
     @NotNull(message = "계좌 번호는 필수 값입니다")
     @NotBlank(message = "계좌 번호는 공백일 수 없습니다.")
+    @Schema(defaultValue = "000000000000",description = "계좌번호")
     private String account;
 
 }
