9e4b3ac78e7275d56999ac95154e74a988acdd0e
2unmi <2unmin4030@gmail.com>
2025-05-21 12:43:19 +0900

    [Docs] Swagger 설정 - 좋아요 관련 api 명세서 작성

--- src/main/java/com/doubleowner/revibe/domain/like/controller/LikeController.java
+++ src/main/java/com/doubleowner/revibe/domain/like/controller/LikeController.java
@@ -5,6 +5,9 @@ import com.doubleowner.revibe.domain.like.service.LikeService;
 import com.doubleowner.revibe.domain.user.entity.User;
 import com.doubleowner.revibe.global.common.dto.CommonResponseBody;
 import com.doubleowner.revibe.global.config.auth.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,8 +25,11 @@ public class LikeController {
 
     private final LikeService likeService;
 
-    // 좋아요(관심상품 등록.해제)
     @PostMapping("/likes")
+    @Operation(summary = "좋아요 API",description = "상품을 좋아요 등록/해제 할수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "좋아요 등록/해제 성공하면 200 OK")
+    })
     public ResponseEntity<CommonResponseBody<?>> doLike(
             @RequestParam(value = "itemId") Long itemId,
             @AuthenticationPrincipal UserDetailsImpl userDetails
@@ -38,6 +44,10 @@ public class LikeController {
 
     // 좋아요(관심상품) 목록 조회
     @GetMapping("/likes")
+    @Operation(summary = "좋아요한 상품 목록 API",description = "좋아요한 상품을 할수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "조회 성공하면 200 OK")
+    })
     public ResponseEntity<CommonResponseBody<List<LikeResponseDto>>> getLikes(
             @AuthenticationPrincipal UserDetailsImpl userDetails)
     {
