88cfe8b3a3783a16681deff3b49878062d1cc15c
2unmi <2unmin4030@gmail.com>
2025-05-21 12:56:03 +0900

    [Docs] Swagger 설정 - 판매 입찰 관련 api 명세서 작성

--- src/main/java/com/doubleowner/revibe/domain/sellbid/controller/SellBidController.java
+++ src/main/java/com/doubleowner/revibe/domain/sellbid/controller/SellBidController.java
@@ -6,6 +6,9 @@ import com.doubleowner.revibe.domain.sellbid.service.SellBidService;
 import com.doubleowner.revibe.domain.user.entity.User;
 import com.doubleowner.revibe.global.common.dto.CommonResponseBody;
 import com.doubleowner.revibe.global.config.auth.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +30,10 @@ public class SellBidController {
      * @return responseDto - 판매입찰 완료 응답 dto
      */
     @PostMapping
+    @Operation(summary = "판매 입찰 등록 API",description = "상품에 대한 판매 입찰 가격을 등록할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "판매 입찰 등록 성공 하면 200 OK") // todo ResponseCode 수정
+    })
     public CommonResponseBody<?> createSellBid(
             @RequestBody @Valid SellBidRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -43,6 +50,10 @@ public class SellBidController {
      * @return  판매입찰 조회 응답 message
      */
     @GetMapping
+    @Operation(summary = "판매 입찰 조회 API",description = "상품에 대한 판매 입찰 가격을 조회할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "판매 입찰 조회 성공 하면 200 OK")
+    })
     public CommonResponseBody<List<SellBidResponseDto>> findBuyBid(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @RequestParam(value = "page", required = false, defaultValue = "0") int page,
@@ -54,6 +65,10 @@ public class SellBidController {
     }
 
     @GetMapping("/{optionId}")
+    @Operation(summary = "옵션 아이디로 입찰 조회 API",description = "상품에 대한 판매 입찰 가격을 옵션아이디로 조회할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "판매 입찰 조회 성공 하면 200 OK")
+    })
     public CommonResponseBody<List<SellBidResponseDto>> findBuyBidByOptionId(
             @PathVariable Long optionId,
             @RequestParam(value = "page", required = false, defaultValue = "0") int page,
@@ -69,6 +84,10 @@ public class SellBidController {
      * @return  판매입찰 취소 응답 message
      */
     @DeleteMapping("/{sellBidId}")
+    @Operation(summary = " 판매 입찰 취소 API",description = "상품에 대한 판매 입찰을 취소할 수 있습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "판매 입찰 취소 성공 하면 200 OK")
+    })
     public CommonResponseBody<?> deleteSellBid(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
             @PathVariable Long sellBidId) {
--- src/main/java/com/doubleowner/revibe/domain/sellbid/dto/SellBidRequestDto.java
+++ src/main/java/com/doubleowner/revibe/domain/sellbid/dto/SellBidRequestDto.java
@@ -4,19 +4,19 @@ import com.doubleowner.revibe.domain.option.entity.Option;
 import com.doubleowner.revibe.domain.sellbid.entity.SellBid;
 import com.doubleowner.revibe.domain.user.entity.User;
 import com.doubleowner.revibe.global.common.enumType.BidStatus;
-import jakarta.validation.constraints.Min;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class SellBidRequestDto {
 
     @NotNull
+    @Schema(defaultValue = "1",description = "옵션 id 입력") //todo 수정
     private Long optionId;
 
     @NotNull
+    @Schema(defaultValue = "20000",description = "판매 입찰 가격")
     private Long price;
 
 
