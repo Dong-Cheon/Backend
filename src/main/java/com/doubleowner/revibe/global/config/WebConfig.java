568e7ffbf04a003f10069a3e273f2c2598bec1a6
2unmi <2unmin4030@gmail.com>
2025-05-28 12:48:20 +0900

    [Chore] 허용 경로 수정

--- src/main/java/com/doubleowner/revibe/global/config/WebConfig.java
+++ src/main/java/com/doubleowner/revibe/global/config/WebConfig.java
@@ -30,7 +30,7 @@ public class WebConfig {
     private final AuthenticationEntryPoint authenticationEntryPoint;
     private final AccessDeniedHandler accessDeniedHandler;
 
-    private static final String[] WHITE_LIST = {"/swagger-ui/**", "/v3/api-docs/**","/api/users/**", "/oauth2/**", "/login/oauth2/code/**", "/oauth/authorize"};
+    private static final String[] WHITE_LIST = {"/swagger-ui/**", "/v3/api-docs/**","/swagger-ui.html","/webjars/**","/api/users/**", "/oauth2/**", "/login/oauth2/code/**", "/oauth/authorize"};
     private static final String[] ADMIN_ONLY_URLS = {"/api/brands", "/api/coupons/**"};
 
     @Bean
