1a62964a28f6e7488978567d987e76e4c63e5574
2unmi <2unmin4030@gmail.com>
2025-05-29 11:15:59 +0900

    [Chore] 배포 서버 Swagger Url 설정

--- src/main/java/com/doubleowner/revibe/global/config/SwaggerConfig.java
+++ src/main/java/com/doubleowner/revibe/global/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +17,16 @@ public class SwaggerConfig {
     private static final String TITLE = "revibe 프로젝트 API 명세서";
     private static final String SECURITY_SCHEME_NAME ="Bearer Authentication";
 
+    @Value("${springdoc.swagger-ui.url}")
+    private String url;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title(TITLE)
                         .version(VERSION))
+                .addServersItem(new Server().url(url))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
                         .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme().name(SECURITY_SCHEME_NAME).type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
